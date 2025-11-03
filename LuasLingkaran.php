8dcd3b56c8ea6051a548fc6e4f6c6b9be2fc78a4
NaylaFarras <yanbintang8@gmail.com>
2025-11-03 22:39:20 +0700

    commit kedua

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -1,25 +1,12 @@
 <?php
 
-namespace App\Math; //definisikan namespace untuk class LuasLingkaran
-
 class LuasLingkaran {
     public const phi = 3.14;
-    private int $jari;
-
-    public function __construct($isiJari = 1) {
-        $this->jari = $isiJari; //16
-    }
-
-    public function tampil($nama = 'ban') {
-        $rumus = LuasLingkaran::phi * $this->jari * $this->jari;
-        echo "Lingkaran {$nama} hasilnya adalah: {$rumus}" ;
-    }
-
-    public static function testing() {
-        echo "ini dari static";
-    }
-
-    public function __destruct() {
-        echo "udah ah cape";
-    }
+    public int $jari;
 }
+
+$lingkaran = new LuasLingkaran();
+$lingkaran->jari = 16;
+
+$rumus = LuasLingkaran::phi * $lingkaran->jari * $lingkaran->jari;
+echo "Hasilnya adalah: ". $rumus;
