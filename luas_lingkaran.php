873ae523e0ecf73c7beb9a08cd1e95a68160e469
NaylaFarras <yanbintang8@gmail.com>
2025-11-03 22:23:05 +0700

    Commit pertama

---
+++ luas_lingkaran.php
@@ -0,0 +1,28 @@
+<?php
+
+class LuasLingkaran {
+    public const phi = 3.14;
+    public int $jari;
+
+    public function __construct($isiJari = 1) {
+        $this->jari = $isiJari;
+    }
+
+    public function tampil($nama = 'ban') {
+        $rumus = LuasLingkaran::phi * $this->jari * $this->jari;
+        echo "Lingkaran {$nama} hasilnya adalah: {$rumus}" ;
+    }
+
+    public static function testing() {
+        echo "ini dari static";
+    }
+
+    public function __destruct() {
+        echo "udah ah cape";
+    }
+}
+
+$lingkaran = new LuasLingkaran(16);
+$lingkaran->tampil('roda'); //panggil method
+
+LuasLingkaran::testing();
